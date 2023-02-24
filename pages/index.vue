de145f1ba68c0f133d09e73d6faec6a7007e78d3
Edwin Boon <edwin.boon@eo.nl>
2023-02-24 10:55:19 +0100

    Fixed type in title

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="text-center my-auto h-full">
     <h1 class="text-3xl text-gray-600">Hi my name is <span>Edwin</span> Boon</h1>
-    <h2 class="text-1xl text-gray-400">And I'm I fullstack developer</h2>
+    <h2 class="text-1xl text-gray-400">And I'm a fullstack developer</h2>
   </main>
 </template>
 
