b4d5b1f612fc7d421ed1767d65075697b8da1f99
Edwin Boon <edwin.boon@eo.nl>
2023-02-24 10:28:52 +0100

    wip navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,10 @@
-<script setup lang="ts">
-
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
+  <div class="flex flex-col md:flex-row">
+    <nav class="h-12 w-full mb-8 md:w-24 md:h-screen bg-slate-500">
+      incons go here
+    </nav>
     <slot />
   </div>
 </template>
